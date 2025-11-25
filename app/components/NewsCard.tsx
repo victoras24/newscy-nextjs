8622a9955def152b292142225b4e3abf9e296ca8
zebra <victoras.isaakidis@sysdocgroup.com>
2025-11-25 20:42:45 +0200

    news card component with mockup text

---
+++ app/components/NewsCard.tsx
@@ -0,0 +1,27 @@
+import "../style/_newsCard.scss";
+
+const NewsCard: React.FC = () => {
+	return (
+		<div>
+			<hr className="h-px my-2 bg-gray-200 border-0 mx-4" />
+			<div className="flex justify-between items-center bg-neutral-primary-soft p-4 rounded-xl md:flex-row mb-3">
+				<div className="">
+					<img
+						className="news-card__image object-cover w-full rounded-xl mr-4"
+						src="https://cyprus-mail.com/image/s640x515/fill/webp/path/wp-content/uploads/2025/11/feature-charalambos-main-The-Landmark-hotel-and-luxury-residence-complex-in-Nicosia-will-host-EU-delegates.jpg"
+						alt=""
+					/>
+				</div>
+				<div className="flex flex-col justify-items-end items-end md:pl-4 leading-normal ">
+					<h5>Category</h5>
+					<h5 className="mb-2 text-lg font-medium text-end hover:cursor-pointer hover:underline underline-offset-2">
+						Wall Street Journal Raises A Blistering Question About RFK Jr.
+					</h5>
+					<span className="text-sm">2 hour</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NewsCard;
