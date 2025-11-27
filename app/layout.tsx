54e3cd0be972b334b264fa64445a8f5a7c215985
zebra <victoras.isaakidis@sysdocgroup.com>
2025-11-27 22:56:12 +0200

    comment out call to perplexity, remove classname from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,13 @@ import { NavigationBar } from "./components/NavigationBar";
 
 export default function RootLayout({
 	children,
-	className,
 }: Readonly<{
 	children: React.ReactNode;
-	className: string;
 }>) {
 	return (
 		<html>
 			<body>
-				<div className={`min-h-screen flex flex-col flex-1 ${className}`}>
+				<div className={`min-h-screen flex flex-col flex-1`}>
 					<NavigationBar />
 					<main className="flex-1 md:px-50">{children}</main>
 				</div>
