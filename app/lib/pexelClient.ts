b4b7fa836260ec0294291a14f7f4b363e09a4d4b
zebra <victoras.isaakidis@sysdocgroup.com>
2025-11-29 14:56:58 +0200

    create pexel client to fetch img by the key word

---
+++ app/lib/pexelClient.ts
@@ -0,0 +1,44 @@
+import supabaseClient from "@/app/lib/supabaseClient";
+import { createClient } from "pexels";
+
+const client = createClient(`${process.env.NEXT_PUBLIC_PEXEL_API}`);
+
+const getImageByKeyWords = async (query: string) => {
+	try {
+		const imageData = await client.photos.search({
+			query,
+			per_page: 1,
+		});
+
+		if ("error" in imageData || imageData.photos.length === 0) {
+			return null;
+		}
+
+		const imageObj = imageData.photos[0];
+
+		const columns = {
+			url: imageObj.src.original,
+			photographer: imageObj.photographer,
+			photographer_url: imageObj.photographer_url,
+			alt: imageObj.alt,
+		};
+
+		const { data, error } = await supabaseClient
+			.from("images")
+			.insert(columns)
+			.select()
+			.single();
+
+		if (data) {
+			return data;
+		}
+
+		if (error) {
+			console.log(error);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export default getImageByKeyWords;
