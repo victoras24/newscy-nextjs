8fc5e9293415f852b41b8082cce541f90c83c1e4
zebra <victoras.isaakidis@sysdocgroup.com>
2025-12-07 21:18:32 +0200

    get the lat and lon helper function

---
+++ app/helper/getLatLon.ts
@@ -0,0 +1,17 @@
+type coordination = {
+	lat: number;
+	lon: number;
+};
+
+export const getLatLon = () => {
+	let coordinations: coordination = {} as coordination;
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition((success) => {
+			coordinations.lat = success.coords.latitude;
+			coordinations.lon = success.coords.longitude;
+		});
+	} else {
+		console.log("Geolocation is not supported in the browser");
+	}
+	return coordinations;
+};
