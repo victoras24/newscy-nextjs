e37b25e67c5aba60c112c183cd921734ac617243
zebra <victoras.isaakidis@sysdocgroup.com>
2025-12-20 20:56:29 +0200

    add padding to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={jetbrainsMono.variable}>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
 			>
 				{children}
 			</body>
