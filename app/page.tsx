5e7cbedab8df2bed36ef98a76a149138d4a9aa09
zebra <victoras.isaakidis@sysdocgroup.com>
2025-12-20 20:57:07 +0200

    order by descending date the news

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CurrentDate from "./components/CurrentDate";
 import Weather from "./components/Weather";
 import supabaseClient from "./lib/supabaseClient";
 import NewsCard from "./components/NewsCard";
+import { LoginForm } from "./components/LoginForm";
 
 const News: React.FC<{ categoryFilter: string }> = ({ categoryFilter }) => {
 	async function LoadNews() {
@@ -10,8 +11,10 @@ const News: React.FC<{ categoryFilter: string }> = ({ categoryFilter }) => {
 					.from("articles")
 					.select("*")
 					.like("category", `%${categoryFilter}%`)
-			: await supabaseClient.from("articles").select("*");
-
+			: await supabaseClient
+					.from("articles")
+					.select("*")
+					.order("date", { ascending: false });
 		if (data) {
 			return (
 				<div>
@@ -40,14 +43,18 @@ const News: React.FC<{ categoryFilter: string }> = ({ categoryFilter }) => {
 				<Weather />
 			</div>
 			<div className="grid grid-cols-1 md:grid-cols-12 md:gap-4">
-				<div className="bg-white rounded-xl md:col-span-6">
+				<div className="bg-white rounded-xl md:col-span-8">
 					<div>
 						<h2 className="p-4">News</h2>
 					</div>
 					{LoadNews()}
 				</div>
-				<div className="bg-white rounded-xl md:col-span-3">Something</div>
-				<div className="bg-white rounded-xl md:col-span-3">Something</div>
+				<div className="bg-white rounded-xl md:col-span-4">
+					<div>
+						<h2 className="p-4">News</h2>
+					</div>
+					<LoginForm />
+				</div>
 			</div>
 		</>
 	);
