b0914491a2abd9e004711aaf13145d89cc3dc98c
zebra <victoras.isaakidis@sysdocgroup.com>
2025-12-21 19:57:38 +0200

    added privacy page for the fb developer

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+const PrivacyPolicy = () => {
+	return (
+		<div>
+			<p>
+				This application does not collect, store, or share personal data from
+				users. Any data processed is used solely to provide app functionality
+				and is not retained. If you have questions or requests related to
+				privacy, please contact: cynewstoday@gmail.com
+			</p>
+		</div>
+	);
+};
+
+export default PrivacyPolicy;
