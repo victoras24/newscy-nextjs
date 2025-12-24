2aaabfeb19d5be74bb0d69576e8284ca05eb10e0
zebra <victoras.isaakidis@sysdocgroup.com>
2025-12-24 23:47:57 +0200

    initial implementation of the load more comp

---
+++ app/components/LoadMore.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import NewsCard from "./NewsCard";
+import { supabaseBrowser } from "../lib/supabaseBrowser";
+
+export const LoadMore = () => {
+	const [page, setPage] = useState<number>(1);
+	const [hasMore, setHasMore] = useState<boolean>(true);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [posts, setPosts] = useState<any[]>([]);
+
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	const loadMoreData = async () => {};
+
+	useEffect(() => {
+		const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+			entries.forEach(async (entry) => {
+				if (entry.isIntersecting && !isLoading && hasMore) {
+					const supabase = supabaseBrowser();
+					setIsLoading(true);
+					const from = page * 10;
+					const to = from + 10 - 1;
+
+					const { data } = await supabase
+						.from("articles")
+						.select("*")
+						.range(from, to)
+						.order("date", { ascending: false });
+
+					if (data) {
+						setPosts((prev: any) => [...prev, ...data]);
+						if (data.length < 10) {
+							setHasMore(false);
+						}
+						setPage((prev) => prev + 1);
+						setIsLoading(false);
+					}
+				}
+			});
+		};
+
+		const options = {
+			root: null,
+			rootMargin: "0px",
+		};
+
+		const observer = new IntersectionObserver(handleIntersect, options);
+		if (ref.current !== null) {
+			observer.observe(ref.current);
+		} else {
+			console.log("Element is not loaded");
+		}
+
+		return () => observer.disconnect();
+	}, []);
+
+	return (
+		<div ref={ref}>
+			<div>
+				{posts &&
+					posts.map((article: any, index: number) => {
+						return (
+							<NewsCard
+								key={index}
+								id={article.id}
+								category={article.category}
+								rewritten_title={article.rewritten_title}
+								summary={article.summary}
+								date={article.date}
+								imageUrl={article.image_url}
+							/>
+						);
+					})}
+			</div>
+			{hasMore && <span>Load more...</span>}
+		</div>
+	);
+};
