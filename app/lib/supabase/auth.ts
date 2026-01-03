816d035640f1a2bb56413e31942432c7e32fce7f
zebra <victoras.isaakidis@sysdocgroup.com>
2026-01-03 22:05:50 +0200

    replace supabase browser client with server client to to access the server auth cookies, initialize a new client each function as it's recommended by the supabase docs

--- app/lib/supabase/auth.ts
+++ app/lib/supabase/auth.ts
@@ -1,9 +1,10 @@
 import { revalidatePath } from "next/cache";
 import supabaseClient from "../supabaseClient";
+import { createClient } from "./server";
 
 export const signUpNewUser = async (formData: FormData) => {
 	"use server";
-	console.log(formData);
+	var supabaseServerClient = await createClient();
 
 	const rawFormData = {
 		email: formData.get("email"),
@@ -12,8 +13,7 @@ export const signUpNewUser = async (formData: FormData) => {
 
 	console.log(rawFormData);
 
-	// did a mistake where i was doing if else error after the (data), in that way i never got the error
-	const { data, error } = await supabaseClient.auth.signUp({
+	const { data, error } = await supabaseServerClient.auth.signUp({
 		email: rawFormData.email as string,
 		password: rawFormData.password as string,
 		options: {
@@ -33,13 +33,13 @@ export const signUpNewUser = async (formData: FormData) => {
 
 export const signInWithEmail = async (formData: FormData) => {
 	"use server";
-
+	var supabaseServerClient = await createClient();
 	const rawFormData = {
 		email: formData.get("email"),
 		password: formData.get("password"),
 	};
 
-	const { data, error } = await supabaseClient.auth.signInWithPassword({
+	const { data, error } = await supabaseServerClient.auth.signInWithPassword({
 		email: rawFormData.email as string,
 		password: rawFormData.password as string,
 	});
@@ -52,3 +52,33 @@ export const signInWithEmail = async (formData: FormData) => {
 		console.log(error.message);
 	}
 };
+
+export async function signOut() {
+	var supabaseServerClient = await createClient();
+	const { error } = await supabaseServerClient.auth.signOut();
+
+	if (error) {
+		console.log(error);
+	}
+}
+
+export async function getUser() {
+	var supabaseServerClient = await createClient();
+	const {
+		data: { user },
+	} = await supabaseServerClient.auth.getUser();
+	let metadata = user?.user_metadata;
+	return metadata;
+}
+
+export async function isUserSignedIn(): Promise<boolean> {
+	const user = await getUser();
+	if (user) return true;
+	return false;
+}
+
+export async function getSession() {
+	var supabaseServerClient = await createClient();
+	const { data } = await supabaseServerClient.auth.getSession();
+	console.log(data);
+}
