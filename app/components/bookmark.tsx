aa8c584e7df3bb92b29ffbbf4d876579bad20a69
victoras24 <esaakidis@gmail.com>
2026-01-10 00:56:44 +0200

    add unsave functionality

--- app/components/bookmark.tsx
+++ app/components/bookmark.tsx
@@ -21,14 +21,30 @@ export const BookmarkComponent: React.FC<BookmarkProps> = ({userId, articleId, i
 							userId,
 							articleId
 						})
-                        console.log("data", data)
+                        
             if (data.status === 201) {
                 setIsSaved(true)
                 router.refresh()
             }
 		}
 
+        async function unsaveArticle(userId: string, articleId: string) {
+        const { data, error } = await supabaseClient
+            .from("saved")
+            .delete()
+            .eq("userId", userId)
+            .eq("articleId", articleId);
+
+        if (error) {
+            console.error("Failed to delete:", error);
+        } else {
+            router.refresh()
+            console.log("Deleted successfully:", data);
+        }
+
+        };
+
     return <div>
-        {isArticleSaved ? <Bookmark onClick={() => saveArticle(userId, articleId)} className="hover:cursor-pointer" fill="white"/> : <Bookmark onClick={() => saveArticle(userId, articleId)} className="hover:cursor-pointer"/>}
+        {isArticleSaved ? <Bookmark onClick={() => unsaveArticle(userId, articleId)} className="hover:cursor-pointer" fill="white"/> : <Bookmark onClick={() => saveArticle(userId, articleId)} className="hover:cursor-pointer"/>}
     </div>
 }
